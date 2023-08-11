5525ef93e4632eceffc17882aa67044d3e1cb15c
Yuiffy <yuiffy@qq.com>
2023-08-11 14:14:10 +0800

    feat: next.js版本增加google adsence代码

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,14 +15,8 @@ export default function Home() {
     return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         {/*<Head>*/}
-        {/*    <Script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></Script>*/}
-        {/*    <Script>*/}
-        {/*        (adsbygoogle = window.adsbygoogle || []).push({*/}
-        {/*        // @ts-ignore*/}
-        {/*        google_ad_client: "ca-pub-7443445828476112",*/}
-        {/*        enable_page_level_ads: true*/}
-        {/*    });*/}
-        {/*    </Script>*/}
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7443445828476112"
+                crossOrigin="anonymous"></Script>
         {/*</Head>*/}
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         {/*<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">*/}
